c7e66803ce0f813ef81a6ee28a00e78ef6097ba6
zhouzewen <a595429717@163.com>
2020-05-28 09:32:33 +0800

    增加了多国语言的功能

---
+++ app/src/main/java/com/test/fic/myapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.test.fic.myapplication;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public  static  final String EXTRA_MESSAGE  = "com.example.myfirstapp.MESSAGE";
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "1111", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this,DisplayMessageActivity.class);
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String message = editText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
+
+
+
+        /*button 按钮事件*/
+
+//        public void button_click(View view){
+//            Intent intent = new Intent(this, DisplayMessageActivity.class);
+//            EditText editText = (EditText) findViewById(R.id.editText);
+//            String message = editText.getText().toString();
+//            intent.putExtra(EXTRA_MESSAGE, message);
+//            startActivity(intent);
+//        }
+    }
+
+
+
+}
