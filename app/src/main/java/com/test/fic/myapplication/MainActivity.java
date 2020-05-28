804a6cd6cc052cc62eaade82ee4aaf7b3abf899c
zhouzewen <a595429717@163.com>
2020-05-28 16:47:55 +0800

    调用其他app和分享数据给其他app

--- app/src/main/java/com/test/fic/myapplication/MainActivity.java
+++ app/src/main/java/com/test/fic/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.test.fic.myapplication;
 
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +13,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public  static  final String EXTRA_MESSAGE  = "com.example.myfirstapp.MESSAGE";
@@ -24,27 +30,46 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "1111", Toast.LENGTH_LONG).show();
 
-                Intent intent = new Intent(MainActivity.this,DisplayMessageActivity.class);
-                EditText editText = (EditText) findViewById(R.id.editText);
-                String message = editText.getText().toString();
-                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
+
+//                Toast.makeText(MainActivity.this, "1111", Toast.LENGTH_LONG).show();
+//
+//                Intent intent = new Intent(MainActivity.this,DisplayMessageActivity.class);
+//                EditText editText = (EditText) findViewById(R.id.editText);
+//                String message = editText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+//                startActivity(intent);
+
+
+//                /*intent 查看地图*/
+//                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+//                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+//                PackageManager packageManager = getPackageManager();
+//                List<ResolveInfo> activities = packageManager.queryIntentActivities(mapIntent, 0);
+//                boolean isIntentSafe = activities.size() > 0;
+//                if (isIntentSafe){
+//                    startActivity(mapIntent);
+//                }
+
+                /*显示分享app的选择界面*/
+//                Intent intent = new Intent(Intent.ACTION_SEND);
+//
+//                String title = getResources().getString(R.string.chooser_title);
+//                Intent chooser = Intent.createChooser(intent, title);
+//                startActivity(chooser);
+
+                /*分享文本内容*/
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
             }
         });
 
 
 
-        /*button 按钮事件*/
 
-//        public void button_click(View view){
-//            Intent intent = new Intent(this, DisplayMessageActivity.class);
-//            EditText editText = (EditText) findViewById(R.id.editText);
-//            String message = editText.getText().toString();
-//            intent.putExtra(EXTRA_MESSAGE, message);
-//            startActivity(intent);
-//        }
     }
 
 
